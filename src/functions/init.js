947845bd344ccdf008a30923a6705f3eb9963660
Ramco <ramazane791@gmail.com>
2024-07-25 09:19:46 +0300

    fixed app uses

--- src/functions/init.js
+++ src/functions/init.js
@@ -4,11 +4,12 @@ const express = require("express");
 
 let init = (app) => {
     let config = loadConfig();
-    loadRoutes(app, config);
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(config.staticDir, express.static(config.staticDir));
-
+    
+    loadRoutes(app, config);
     app.listen(config.port, () => {
         console.log(`Example app listening at http://localhost:${config.port}`);
     });
